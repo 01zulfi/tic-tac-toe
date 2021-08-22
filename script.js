5893cc8b9a7d72949b138f904c4001b27e0ea943
Zulfiqar Ali <01zulfiqarali@gmail.com>
2021-08-23 00:27:25 +0500

    More work on DisplayController module

--- script.js
+++ script.js
@@ -1,18 +1,50 @@
-const GameBoard = (() => {
-    const gameBoard = {
-        array: ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'],
-    } 
-    return {
-        gameBoard: gameBoard.array
-    }
-})()
-
+//Dependencies? Yes. No for now.
 const DisplayController = (() => {
+    const array = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'O'];
     const gameBoardDiv = document.querySelector('#gameBoardDiv');
-    const body = document.querySelector('#body');
-    gameBoardDiv.textContent = GameBoard.gameBoard;
-    body.appendChild(gameBoardDiv);
-})
+    const display = {
+        grid: function() {
+            for (let i = 0; i < 9; i++) {
+                const gameBoardSquare = document.createElement('div');
+                gameBoardSquare.id = 'gameBoardSquare';
+                gameBoardDiv.appendChild(gameBoardSquare);
+            }
+        },
+        marker: function() {
+            const gameBoardSquareNodeList = document.querySelectorAll('#gameBoardSquare');
+            for (let i = 0; i < 9; i++) {
+                gameBoardSquareNodeList[i].textContent = array[i];
+            }
+        }
+    }
+    const gamePlay = {
+        set: function() {
+            document.querySelectorAll('#gameBoardSquare').forEach( index =>
+                index.addEventListener('click', () => {
+                index.textContent = 'O';
+            }))
+        }
+    }
+    return {
+        grid: display.grid(),
+        //marker: display.marker(),
+        gamePlay: gamePlay,
+        }
+})();
+
+DisplayController.gamePlay.set();
+
+// const GameBoard = (() => { //control array
+//     const gameBoard = {
+//         array: ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'O'],
+//     }
+//     DisplayController.gamePlay.set();
+//     return {
+//         gameBoard
+//     }
+// })()
+
+
 
 function Player(name, marker) {
     return {
